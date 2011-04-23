46dd0f9b16afe5f41173201268049051a3004254
loga.j <logaj@ubuntu.(none)>
2011-04-23 01:20:47 +0100

    program is now more interactive

--- c2f.rb
+++ c2f.rb
@@ -1,5 +1,6 @@
-celsius = 100
-fahrenheit = (celsius * 9 / 5) + 32
-puts "The result is: "
-puts fahrenheit
+print "Hello. Please enter a Celsius value: "
+celsius = gets
+fahrenheit = (celsius.to_i * 9 / 5) + 32
+print "The Fahrenheit equivalent is "
+print fahrenheit
 puts "."
