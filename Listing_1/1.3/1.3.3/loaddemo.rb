ae5b0190f0f226f6264047b367643395a7b9f5e1
Logi Louges <loga.jegede@gmail.com>
2011-05-06 22:06:58 +0100

    require-ing practice

--- Listing_1/1.3/1.3.3/loaddemo.rb
+++ Listing_1/1.3/1.3.3/loaddemo.rb
@@ -1,3 +1,3 @@
 puts "This is the first (master) program file."
-load "loadee.rb"
+require "./loadee" #Ruby 1.9.2 no longer has the current dir in the load path so ./ or $:<< would have to be used.
 puts "And back again to the first file."
